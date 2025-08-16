e1df6ab04860db2ca945387151b769118e6ef92b
saspog <sapcat62@gmail.com>
2025-08-16 00:03:22 -0500

    Adding new pages and getting navigation set up.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import { NavLink, Routes, Route } from 'react-router-dom'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
 
-  return (
-    <div className='app-container'>
-      <h1>Sasha P.</h1>
-    </div>
-  )
-}
+const App = () => (
+  <div className='app'>
+    <h1>Sasha P.</h1>
+    <Navigation />
+    <Main />
+  </div>
+)
+
+const Navigation = () => (
+  <nav>
+    <ul>
+      <li><NavLink to="/">Home</NavLink></li>
+      <li><NavLink to="/about">About</NavLink></li>
+      <li><NavLink to="/contact">Contact</NavLink></li>
+    </ul>
+  </nav>
+)
+
+const Main = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />} />
+  </Routes>
+)
 
 export default App
---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const About = () => (
+  <div className='about'>
+    <h1>About Me</h1>
+    <p>Ipsum dolor dolorem consectetur est velit fugiat...</p>
+  </div>
+)
+
+export default About
+
